e7146af7406951c005e7a84023084afd51b71fb3
koola97620 <koola97620@nate.com>
2022-07-02 01:13:10 +0900

    Initial commit

---
+++ src/main/java/com/example/mymileageapi/MyMileageApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymileageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMileageApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMileageApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mymileageapi/MyMileageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymileageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMileageApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
