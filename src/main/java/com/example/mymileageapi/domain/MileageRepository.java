a8732dbfbd529826b186300555df8836f561c0b8
koola97620 <koola97620@nate.com>
2022-07-09 09:21:05 +0900

    feat: Lock 옵션 추가

--- src/main/java/com/example/mymileageapi/domain/MileageRepository.java
+++ src/main/java/com/example/mymileageapi/domain/MileageRepository.java
@@ -1,10 +1,13 @@
 package com.example.mymileageapi.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
+import javax.persistence.LockModeType;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface MileageRepository extends JpaRepository<Mileage, Long> {
+    @Lock(value = LockModeType.OPTIMISTIC)
     Optional<Mileage> findByUserId(UUID userId);
 }
